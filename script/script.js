8f81c541724fe9ac3fd4eb07fc05482c6a56bc9e
Minh <hlminh2000@gmail.com>
2016-06-16 18:18:54 -0400

    sets up typescript

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function $(id) {
+    return document.querySelectorAll(id);
+}
+function log(arg) {
+    return console.log(arg);
+}
+var OptionButton = (function () {
+    function OptionButton() {
+    }
+    return OptionButton;
+}());
+window.addEventListener('load', function () {
+});
---
+++ script/script.ts
@@ -0,0 +1,23 @@
+
+//basic utilities
+function $(id:string)
+{
+  return document.querySelectorAll(id);
+}
+
+function log(arg:any)
+{
+  return console.log(arg);
+}
+
+class OptionButton{
+  id:string;
+  tagId:string;
+  constructor(){
+
+  }
+}
+
+window.addEventListener('load', () => {
+  
+});
