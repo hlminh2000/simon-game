76479a053afda7da58aeb88aa85294df52819892
Minh <hlminh2000@gmail.com>
2016-06-18 14:53:28 -0400

    starts on scripting

--- script/script.js
+++ script/script.js
@@ -1,13 +1,82 @@
 function $(id) {
-    return document.querySelectorAll(id);
+    return document.getElementById(id);
 }
-function log(arg) {
-    return console.log(arg);
+function $$(className) {
+    return Array.prototype.slice.call(document.getElementsByClassName(className));
 }
-var OptionButton = (function () {
-    function OptionButton() {
+var StageBox = (function () {
+    function StageBox(element) {
+        var _this = this;
+        this.element = element;
+        this.resetAudio();
+        this.element.addEventListener('click', function () {
+            _this.blink();
+        });
     }
-    return OptionButton;
+    StageBox.prototype.resetAudio = function () {
+        if (this.audio != null) {
+            delete this.audio;
+        }
+        this.audio = new Audio(this.element.id == 'x' ? '/assets/sound/C3.mp3'
+            : this.element.id == 'square' ? '/assets/sound/E3.mp3'
+                : this.element.id == 'circle' ? '/assets/sound/G3.mp3'
+                    : this.element.id == 'triangle' ? '/assets/sound/B3.mp3'
+                        : null);
+    };
+    StageBox.prototype.onClick = function () {
+        this.blink();
+    };
+    StageBox.prototype.blink = function () {
+        var _this = this;
+        this.resetAudio();
+        this.audio.play();
+        this.element.classList.add('blinking');
+        this.element.children[0].classList.add('blinking');
+        setTimeout(function () {
+            _this.element.classList.remove('blinking');
+            _this.element.children[0].classList.remove('blinking');
+        }, 400);
+        console.log(this.element.id);
+    };
+    return StageBox;
 }());
+var GameState = (function () {
+    function GameState(initialState) {
+        this.playerTurn = initialState.playerTurn;
+        this.isPlaying = initialState.isPlaying;
+        this.strictMode = initialState.strictMode;
+    }
+    return GameState;
+}());
+function loadGame(state, boxes) {
+    var delay = 2000;
+    var timer = setTimeout(function () {
+        if (!state.isPlaying) {
+            return;
+        }
+        else {
+            console.log(state);
+        }
+    }, delay);
+    loadGame(state, boxes);
+}
 window.addEventListener('load', function () {
+    var a_StageBox = $$('stage-box').map(function (element) {
+        return new StageBox(element);
+    });
+    var gameState = new GameState({
+        playerTurn: false,
+        isPlaying: false,
+        strictMode: false,
+        currentSequence: [],
+    });
+    $('strictModeToggle').addEventListener('click', function () {
+        gameState.strictMode = !gameState.strictMode;
+        $('strictModeToggle').innerText = gameState.strictMode ? 'ON' : 'OFF';
+    });
+    $('startButton').addEventListener('click', function () {
+        gameState.isPlaying = !gameState.isPlaying;
+        $('startButton-text').innerText = gameState.isPlaying ? 'STOP' : 'START';
+        loadGame(gameState, a_StageBox);
+    });
 });
--- script/script.ts
+++ script/script.ts
@@ -1,23 +1,98 @@
-
-//basic utilities
-function $(id:string)
-{
-  return document.querySelectorAll(id);
+function $(id:string){
+   return document.getElementById(id);
 }
 
-function log(arg:any)
-{
-  return console.log(arg);
+function $$(className:string){
+   return Array.prototype.slice.call(document.getElementsByClassName(className));
 }
 
-class OptionButton{
-  id:string;
-  tagId:string;
-  constructor(){
+class StageBox{
+   element:HTMLElement;
+   audio:HTMLAudioElement;
 
-  }
+   constructor(element:HTMLElement){
+      this.element = element;
+      this.resetAudio();
+
+      this.element.addEventListener('click', ()=>{
+      this.blink();
+      });
+   }
+   resetAudio(){
+      if(this.audio != null){
+         delete this.audio;
+      }
+      this.audio = new Audio(
+         this.element.id == 'x' ? '/assets/sound/C3.mp3'
+            : this.element.id == 'square' ? '/assets/sound/E3.mp3'
+               : this.element.id == 'circle' ? '/assets/sound/G3.mp3'
+                  : this.element.id == 'triangle' ? '/assets/sound/B3.mp3'
+                     : null
+      );
+   }
+   onClick(){
+      this.blink();
+   }
+   blink(){
+      this.resetAudio();
+      this.audio.play();
+      this.element.classList.add('blinking');
+      this.element.children[0].classList.add('blinking');
+      setTimeout(()=>{
+         this.element.classList.remove('blinking');
+         this.element.children[0].classList.remove('blinking');
+      }, 400);
+      console.log(this.element.id);
+   }
+}
+
+class GameState{
+   public playerTurn:boolean;
+   public isPlaying:boolean;
+   public strictMode:boolean;
+   public currentSequence:Array<string>;
+   constructor(initialState:GameState){
+      this.playerTurn = initialState.playerTurn;
+      this.isPlaying = initialState.isPlaying;
+      this.strictMode = initialState.strictMode;
+   }
+}
+
+function loadGame(state: GameState, boxes:Array<StageBox>)
+{
+   // var delay:number = 10000;
+   let delay = 2000;
+   let timer = setTimeout(()=>{
+      if(!state.isPlaying){
+         return;
+      }  else  {
+         console.log(state);
+      }
+   }, delay);
+   loadGame(state, boxes);
 }
 
 window.addEventListener('load', () => {
-  
+   let a_StageBox:Array<StageBox> = $$('stage-box').map((element:HTMLElement)=>{
+      return new StageBox(element);
+   });
+
+   var gameState = new GameState({
+      playerTurn      : false,
+      isPlaying       : false,
+      strictMode      : false,
+      currentSequence : [],
+   });
+
+   $('strictModeToggle').addEventListener('click' , () => {
+      gameState.strictMode = !gameState.strictMode;
+      $('strictModeToggle').innerText = gameState.strictMode ? 'ON' : 'OFF';
+   });
+
+   $('startButton').addEventListener('click' , () => {
+      gameState.isPlaying = !gameState.isPlaying;
+      $('startButton-text').innerText = gameState.isPlaying ? 'STOP' : 'START';
+      loadGame(gameState, a_StageBox);
+   });
+
 });
