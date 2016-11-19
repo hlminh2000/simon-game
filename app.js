34b436f60664c52fad93efd83016296a25e0650f
Minh <hlminh2000@gmail.com>
2016-11-18 21:10:40 -0500

    fixes port issue

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var appRout = require('./server/router.js');
 
 var port = process.env.PORT || 3000;
 
-appRout.router.listen('port', function () {
+appRout.router.listen(port, function () {
   console.log('Example app listening on port ' + port)
 });
